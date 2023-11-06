17a36c3ef5493e48638ede0ece742ce686d18e12
Charan <charankulal0241@gmail.com>
2023-11-06 17:53:44 +0530

    Learnt context passing.

--- studentsync/base/views.py
+++ studentsync/base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+rooms=[
+    {'id':1,'name':'Lets learn Python'},
+    {'id':2,'name':'Design with me'},
+    {'id':3,'name':'Front end Developers Club'},
+]
+
 def home(request):
-    return render(request,'home.html')
+    context={'rooms':rooms}
+    return render(request,'home.html',context)
 
 
 def room(request):
