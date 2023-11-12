75a6b07f79960b5aaf3c9916291fe2ec497c9fb2
Charan <charankulal0241@gmail.com>
2023-11-12 12:19:22 +0530

    Updated the databse with room form.

--- studentsync/base/views.py
+++ studentsync/base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Room
-
+from .forms import RoomForm
 
 def home(request):
     rooms=Room.objects.all()
@@ -15,5 +15,11 @@ def room(request,pk):
     return render(request,'base/room.html',context)
 
 def createRoom(request):
-    context={}
+    form=RoomForm
+    if request.method=="POST":
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
     return render(request,'base/room_form.html',context)
