08e366451805a4da2a0abbe51b4ca971993b10b4
Charan <charankulal0241@gmail.com>
2023-11-18 21:15:38 +0530

    Completed create room functionality.

--- studentsync/base/views.py
+++ studentsync/base/views.py
@@ -71,14 +71,18 @@ def room(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm
+    topics= Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room=form.save(commit=False)
-            room.host=request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name=request.POST.get('topic')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+    context = {'form': form,'topics':topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -86,7 +90,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics= Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed to modify!!')
 
@@ -95,7 +99,7 @@ def updateRoom(request, pk):
         if form.is_valid():
             form.save()
             return redirect("home")
-    context = {"form": form}
+    context = {"form": form,"topics":topics}
     return render(request, 'base/room_form.html', context)
 
 
