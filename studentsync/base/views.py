94af5aa9d39935d9a0c4a5b3bab6020f8423e7cf
Charan <charankulal0241@gmail.com>
2023-11-21 19:50:39 +0530

    Created new user model.

--- studentsync/base/views.py
+++ studentsync/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
