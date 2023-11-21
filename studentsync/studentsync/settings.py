f1f0a677cb84a1edf5b5c55256dc37cc8dfbe198
Charan <charankulal0241@gmail.com>
2023-11-21 18:50:22 +0530

    Changed project structure and deleted database inorder to add custom user model.

--- studentsync/studentsync/settings.py
+++ studentsync/studentsync/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
-    'rest_framework',
+    
     'corsheaders',
 ]
+
+AUTH_USER_MODEL='base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
